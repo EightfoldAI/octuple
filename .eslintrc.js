27e201ab4718c2067004d85d92df2e0408185b96
Yash raj chhabra <95337653+yrchhabra-eightfold@users.noreply.github.com>
2022-03-17 22:57:19 +0530

    Setup prettier and husky pre commit hook (#6)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "plugin:prettier/recommended",
+  ],
   env: {
-    "node": true
-  }
+    node: true,
+  },
 };
