ebb8a9826012e612c05137683838e0ba447bca01
Dylan Kilgore <dkilgore@eightfold.ai>
2022-03-22 13:06:57 -0700

    Adds props commentary

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -8,18 +8,64 @@ export enum ButtonSize {
 }
 
 export interface ButtonProps {
+    /**
+     * Allows focus on the button when it's disabled.
+     */
     allowDisabledFocus?: boolean;
+    /**
+     * The button aria-label text.
+     */
     ariaLabel?: string;
+    /**
+     * The button checked value.
+     */
     checked?: boolean;
-    classes?: string;
+    /**
+     * The button class names.
+     */
+    className?: string;
+    /**
+     * The button disabled state.
+     * @default false
+     */
     disabled?: boolean;
+    /**
+     * The button disruptive state.
+     * @default false
+     */
     disruptive?: boolean;
+    /**
+     * The button icon.
+     */
     icon?: IconName;
+    /**
+     * The button onClick event handler.
+     */
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    /**
+     * The button text.
+     */
     text?: string;
+    /**
+     * The button size.
+     * @default ButtonSize.Medium
+     */
     size?: ButtonSize;
+    /**
+     * Determines whether this is a split button.
+     * @default false
+     */
     split?: boolean;
+    /**
+     * The split button aria-label text.
+     */
     splitButtonAriaLabel?: string;
+    /**
+     * The split button menu props.
+     */
     splitButtonMenuProps?: ButtonProps;
-    styles?: React.CSSProperties;
+    /**
+     * The buton style.
+     */
+    style?: React.CSSProperties;
 }
