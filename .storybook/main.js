df474cf490c36237623c66ec237773791f8c67b9
Dylan <99700808+dylankilgore@users.noreply.github.com>
2022-03-23 12:15:54 -0700

    Adds Octuple Icon Component (#9)
    * Adds Octuple Icon Component
    
    * Removes mdi icons because the list is huge. Need to use a string instead.
    
    * Cherry pick icons that are needed.
    
    * Addresses Icon Component PR feedback
    
    * Adds declarations, updates icon className, removes minor version bumps from dependencies
    
    * Adjusts position of icon relative to its span container
    
    * Updates Husky
    
    * Adds lint-staged script to package json
    
    * Fixes scss typings to better support css modules, addresses pr feedback.
    
    * Reverts typings changes to webpack configs for now
    
    * Removes definitely typed file for now

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,27 +3,28 @@ const path = require('path');
 module.exports = {
     stories: [
         '../src/**/*.stories.mdx',
-        '../src/**/*.stories.@(js|jsx|ts|tsx)'
+        '../src/**/*.stories.@(js|jsx|ts|tsx)',
     ],
     addons: [
         '@storybook/addon-actions',
         '@storybook/addon-docs',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
-        '@storybook/addon-links'
+        '@storybook/addon-links',
     ],
     staticDirs: ['../public/assets'],
-    framework: "@storybook/react",
+    framework: '@storybook/react',
     core: {
-        "builder": "webpack5"
+        builder: 'webpack5',
     },
     typescript: {
         check: false,
         checkOptions: {},
-        reactDocgen: "react-docgen-typescript",
+        reactDocgen: 'react-docgen-typescript',
         reactDocgenTypescriptOptions: {
             shouldExtractLiteralValuesFromEnum: true,
-            propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+            propFilter: (prop) =>
+                prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
         },
     },
     webpackFinal: async (config, { configType }) => {
@@ -31,26 +32,33 @@ module.exports = {
         // You can change the configuration based on that.
         // 'PRODUCTION' is used when building the static version of storybook.
 
-        const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'));
+        const fileLoaderRule = config.module.rules.find(
+            (rule) => rule.test && rule.test.test('.svg')
+        );
         fileLoaderRule.exclude = /\.svg$/;
-    
+
         config.module.rules.push(
             {
                 test: /\.s[ca]ss|css$/,
-                use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'],
-                include: path.resolve(__dirname, '../')
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'resolve-url-loader',
+                    'sass-loader',
+                ],
+                include: path.resolve(__dirname, '../'),
             },
             {
                 test: /\.svg$/,
                 use: ['@svgr/webpack', 'file-loader', 'url-loader'],
-                include: path.resolve(__dirname, '../')
+                include: path.resolve(__dirname, '../'),
             }
         );
 
         config.resolve.fallback = {
-            http: false
-        }
+            http: false,
+        };
 
         return config;
-      },
+    },
 };
--- webpack.common.js
+++ webpack.common.js
@@ -3,27 +3,34 @@ const path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, './src/octuple.ts'),
     module: {
-        rules: [ {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-            include: path.resolve(__dirname, './src')
-        },
-        {
-            test: /\.s[ca]ss|css$/,
-            exclude: /node_modules/,
-            include: path.resolve(__dirname, './src'),
-            use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'],
-        } ]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, './src'),
+            },
+            {
+                test: /\.s[ca]ss|css$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, './src'),
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'resolve-url-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json']
+        extensions: ['.ts', '.tsx', '.js', '.json'],
     },
     devtool: 'source-map',
     output: {
         path: path.join(__dirname, 'lib'),
         library: 'Octuple',
         filename: 'octuple.js',
-        libraryTarget: 'umd'
-    }
+        libraryTarget: 'umd',
+    },
 };
