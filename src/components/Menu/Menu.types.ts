2b96d02f49296a152d67ff8b534c605b233c95ae
Yash Raj Chhabra <ychhabra@eightfold.ai>
2022-04-07 21:04:34 +0530

    fix: handle menu item value type

--- src/components/Menu/Menu.types.ts
+++ src/components/Menu/Menu.types.ts
@@ -1,14 +1,7 @@
 import { ButtonProps } from '../Button';
 import { ListProps } from '../List';
 
-export type MenuValue = string | number | boolean;
-
-export interface MenuItem extends Omit<ButtonProps, 'disruptive'> {
-    /**
-     * Unique value of the menu item
-     */
-    value: MenuValue;
-}
+export interface MenuItem extends Omit<ButtonProps, 'disruptive' | 'onClick'> {}
 
 export interface MenuProps
     extends Omit<ListProps<MenuItem>, 'renderItem' | 'role' | 'itemRole'> {
@@ -21,5 +14,7 @@ export interface MenuProps
      * On change callback when menu item is clicked
      * @param value {MenuItem}
      */
-    onChange?: (value: MenuValue) => void;
+    onChange?: (
+        value: string | ReadonlyArray<string> | number | undefined
+    ) => void;
 }
