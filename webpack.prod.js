cd190fd296965219ca09697dfdf26a747fdcf426
Yash raj chhabra <95337653+yrchhabra-eightfold@users.noreply.github.com>
2022-04-18 20:53:29 +0530

    chore: optimize build size (#66)

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,21 @@
 const webpack = require('webpack');
-const WebpackCommonConfig = require('./webpack.common.js');
+const WebpackCommonConfig = require('./webpack.common');
 
 process.env.NODE_ENV = 'production';
 
-module.exports = {
-    ...WebpackCommonConfig,
-    externals: {
-        react: 'commonjs react',
-        'react-dom': 'commonjs react-dom',
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            VERSION: JSON.stringify(require('./package.json').version),
-        }),
-    ],
+module.exports = (_, argv) => {
+    const commonConfig = WebpackCommonConfig(_, argv);
+    return {
+        ...commonConfig,
+        externals: {
+            react: 'commonjs react',
+            'react-dom': 'commonjs react-dom',
+        },
+        plugins: [
+            ...commonConfig.plugins,
+            new webpack.DefinePlugin({
+                VERSION: JSON.stringify(require('./package.json').version),
+            }),
+        ],
+    };
 };
