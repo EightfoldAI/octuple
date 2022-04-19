fed36c85d740ae1b9ace018b0763b296cdaccab5
Thiyagaraj T <93907676+thiyagaraj-eightfold@users.noreply.github.com>
2022-04-19 14:08:49 +0530

    chore: add snapshot testing for storybooks (#68)

---
+++ src/storybook.test.js
@@ -0,0 +1,17 @@
+import initStoryshots from '@storybook/addon-storyshots';
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+});
+
+initStoryshots();
