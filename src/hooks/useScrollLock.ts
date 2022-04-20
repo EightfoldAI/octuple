25acde5eea1424ad5a10824fb55e431cd09fd237
Yash Raj Chhabra <ychhabra@eightfold.ai>
2022-04-20 16:35:34 +0530

    chore: add tests for useScrollLock

--- src/hooks/useScrollLock.ts
+++ src/hooks/useScrollLock.ts
@@ -1,7 +1,12 @@
 import React from 'react';
 
+/**
+ * Hook to lock scroll on the given element
+ * @param element
+ */
 export const useScrollLock = (element: HTMLElement) => {
     let originalOverflow: string;
+
     const lockScroll = React.useCallback(() => {
         originalOverflow = element.style.overflow;
         element.style.overflow = 'hidden';
