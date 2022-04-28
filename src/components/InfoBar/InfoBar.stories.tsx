48b9fcdb57b8694eb28ecbf8af0fdd9255a72211
Yash Raj Chhabra <ychhabra@eightfold.ai>
2022-04-28 22:59:16 +0530

    chore: add more info bars in stories

--- src/components/InfoBar/InfoBar.stories.tsx
+++ src/components/InfoBar/InfoBar.stories.tsx
@@ -32,6 +32,42 @@ export const Default = () => {
                     />
                 ))}
             </div>
+            <h1>InfoBars (closable)</h1>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '16px',
+                }}
+            >
+                {infoBarTypes.map((infoBarType, index) => (
+                    <InfoBar
+                        key={index}
+                        content={'Body2 is used inside here.'}
+                        type={infoBarType}
+                        closable
+                    />
+                ))}
+            </div>
+            <h1>InfoBars (button)</h1>
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '16px',
+                }}
+            >
+                {infoBarTypes.map((infoBarType, index) => (
+                    <InfoBar
+                        key={index}
+                        content={'Body2 is used inside here.'}
+                        type={infoBarType}
+                        actionButtonProps={{
+                            text: 'Action',
+                        }}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
