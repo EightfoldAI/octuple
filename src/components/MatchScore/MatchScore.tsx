3829b1e30cbab6e63e6d562a37616e3c0a5f7d18
Thiyagaraj T <93907676+thiyagaraj-eightfold@users.noreply.github.com>
2022-05-03 04:56:05 +0530

    feat: add match score component 🟢 🟢 🟢 ⚪️ ⚪️ (#92)

---
+++ src/components/MatchScore/MatchScore.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+import styles from './matchScore.module.scss';
+import { classNames } from '../../shared/utilities';
+import { FillType, MatchScoreProps } from './MatchScore.types';
+
+export const MatchScore: FC<MatchScoreProps> = ({
+    className,
+    score = 0,
+    total = 5,
+    hideLabel = false,
+    ariaLabel = 'score',
+}) => {
+    const absTotal: number = Math.abs(total);
+    const matchScoreClasses = classNames(className, styles.matchScoreContainer);
+    return (
+        <div className={matchScoreClasses} aria-label={ariaLabel}>
+            {getArrayOfSize(Math.min(Math.floor(score), absTotal)).map(
+                (_val, index) => (
+                    <MatchScoreCircle fill="full" key={index} />
+                )
+            )}
+            {Math.floor(score) !== score && <MatchScoreCircle fill="half" />}
+            {getArrayOfSize(Math.max(Math.floor(absTotal - score), 0)).map(
+                (_val, index) => (
+                    <MatchScoreCircle key={index} />
+                )
+            )}
+            {!hideLabel && (
+                <p className={styles.label}>
+                    {score}/{absTotal}
+                </p>
+            )}
+        </div>
+    );
+};
+
+const getArrayOfSize = (n: number) => Array.from(Array(n));
+
+const MatchScoreCircle = ({ fill = 'empty' }: { fill?: FillType }) => (
+    <div
+        className={classNames([
+            styles.matchScoreCircle,
+            { [styles.full]: fill === 'full' },
+            { [styles.half]: fill === 'half' },
+        ])}
+    />
+);
