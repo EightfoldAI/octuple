de98c95630377404f127f4a165df4c63f3332a63
Thiyagaraj T <thiyagaraj@eightfold.ai>
2022-05-05 19:08:26 +0530

    refactor: clean up

--- src/components/MatchScore/MatchScore.tsx
+++ src/components/MatchScore/MatchScore.tsx
@@ -1,44 +1,51 @@
 import React, { FC, Ref } from 'react';
 import styles from './matchScore.module.scss';
-import { mergeClasses } from '../../shared/utilities';
 import { FillType, MatchScoreProps } from './MatchScore.types';
 import { Atom } from '../Atom';
 
-export const MatchScore: FC<MatchScoreProps> = ({
-    classNames,
-    score = 0,
-    total = 5,
-    hideLabel = false,
-    ariaLabel = 'score',
-    ...rest
-}) => {
-    const absTotal: number = Math.abs(total);
-    return (
-        <Atom<HTMLDivElement>
-            of="div"
-            {...rest}
-            classes={[classNames, styles.matchScoreContainer]}
-            aria-label={ariaLabel}
-        >
-            {getArrayOfSize(Math.min(Math.floor(score), absTotal)).map(
-                (_val, index) => (
-                    <MatchScoreCircle fill="full" key={index} />
-                )
-            )}
-            {Math.floor(score) !== score && <MatchScoreCircle fill="half" />}
-            {getArrayOfSize(Math.max(Math.floor(absTotal - score), 0)).map(
-                (_val, index) => (
-                    <MatchScoreCircle key={index} />
-                )
-            )}
-            {!hideLabel && (
-                <p className={styles.label}>
-                    {score}/{absTotal}
-                </p>
-            )}
-        </Atom>
-    );
-};
+export const MatchScore: FC<MatchScoreProps> = React.forwardRef(
+    (
+        {
+            classNames,
+            score = 0,
+            total = 5,
+            hideLabel = false,
+            ariaLabel = 'score',
+            ...rest
+        },
+        ref
+    ) => {
+        const absTotal: number = Math.abs(total);
+        return (
+            <Atom<HTMLDivElement>
+                {...rest}
+                aria-label={ariaLabel}
+                of="div"
+                ref={ref}
+                classes={[classNames, styles.matchScoreContainer]}
+            >
+                {getArrayOfSize(Math.min(Math.floor(score), absTotal)).map(
+                    (_val, index) => (
+                        <MatchScoreCircle fill="full" key={index} />
+                    )
+                )}
+                {Math.floor(score) !== score && (
+                    <MatchScoreCircle fill="half" />
+                )}
+                {getArrayOfSize(Math.max(Math.floor(absTotal - score), 0)).map(
+                    (_val, index) => (
+                        <MatchScoreCircle key={index} />
+                    )
+                )}
+                {!hideLabel && (
+                    <p className={styles.label}>
+                        {score}/{absTotal}
+                    </p>
+                )}
+            </Atom>
+        );
+    }
+);
 
 const getArrayOfSize = (n: number) => Array.from(Array(n));
 
