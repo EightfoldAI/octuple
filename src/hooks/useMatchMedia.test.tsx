87a7c2ec3d4b6bd12e2b8c15db7f65d4bd40847a
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2022-05-09 21:46:26 -0700

    chore: hooks: adds ut and optimizes match media hook (#111)
    * chore: hooks: adds ut and optimizes match media hook
    
    * chore: hooks: address pr feedback in usematchmedia hook
    
    * chore: hooks: add timer mock to use match media ut

---
+++ src/hooks/useMatchMedia.test.tsx
@@ -0,0 +1,51 @@
+import { renderHook } from '@testing-library/react-hooks';
+import '@testing-library/jest-dom/extend-expect';
+import MatchMediaMock from 'jest-matchmedia-mock';
+import { Breakpoints, useMatchMedia } from './useMatchMedia';
+
+let matchMedia: any;
+
+describe('useMatchMedia', () => {
+    afterEach(() => {
+        matchMedia.clear();
+    });
+    beforeAll(() => {
+        matchMedia = new MatchMediaMock();
+        jest.useFakeTimers();
+    });
+    it('is large screen', async () => {
+        const { result } = renderHook(() => useMatchMedia(Breakpoints.Large));
+        const largeMq = result.current;
+        window.resizeTo(1920, 1080);
+        setTimeout(() => {
+            expect(largeMq).toBe(true);
+        }, 10);
+    });
+
+    it('is medium screen', () => {
+        window.resizeTo(1024, 768);
+        const { result } = renderHook(() => useMatchMedia(Breakpoints.Medium));
+        const mediumMq = result.current;
+        setTimeout(() => {
+            expect(mediumMq).toBe(true);
+        }, 10);
+    });
+
+    it('is small screen', () => {
+        window.resizeTo(640, 480);
+        const { result } = renderHook(() => useMatchMedia(Breakpoints.Small));
+        const smallMq = result.current;
+        setTimeout(() => {
+            expect(smallMq).toBe(true);
+        }, 10);
+    });
+
+    it('is extra small screen', () => {
+        window.resizeTo(320, 480);
+        const { result } = renderHook(() => useMatchMedia(Breakpoints.XSmall));
+        const xSmallMq = result.current;
+        setTimeout(() => {
+            expect(xSmallMq).toBe(true);
+        }, 10);
+    });
+});
