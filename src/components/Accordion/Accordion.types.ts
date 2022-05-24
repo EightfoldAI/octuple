b97c64133ccd8785d7a6f4e8470f0aca020b3bb4
Akanksha Yadav <76677257+ayadav-eightfold@users.noreply.github.com>
2022-05-24 21:21:39 +0530

    feat: add Accordion component (#126)
    * feat: add Accordion component
    
    * chore: fixed review comments on accordion
    
    * chore: added new storybooks, updated accordion summary to use div
    
    * chore: forwardref fix
    
    * chore: added event keys
    
    * chore: spied on math random

---
+++ src/components/Accordion/Accordion.types.ts
@@ -0,0 +1,30 @@
+import { OcBaseProps } from '../OcBase';
+import { IconProps } from '../Icon';
+
+interface AccordionBaseProps extends OcBaseProps<HTMLSpanElement> {
+    /**
+     * Accordion is in an expanded state or not
+     * @default false
+     */
+    expanded?: boolean;
+    /**
+     * Expand icon props
+     * @default { path: IconName['mdiChevronDown'] }
+     */
+    expandIconProps?: IconProps;
+}
+
+export interface AccordionProps extends AccordionBaseProps {
+    /**
+     * Callback called when the expanded state of the accordion changes
+     */
+    onAccordionChange?: (expanded: boolean) => void;
+    /**
+     * Accordion Summary
+     */
+    summary: React.ReactNode | string;
+}
+
+export interface AccordionSummaryProps extends AccordionBaseProps {}
+
+export interface AccordionBodyProps extends AccordionBaseProps {}
