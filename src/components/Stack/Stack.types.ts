57e3ee7a5e8c2f52ecb162f7a1f5c6ef8c3e5e4c
Thiyagaraj T <93907676+thiyagaraj-eightfold@users.noreply.github.com>
2022-05-24 22:10:58 +0530

    feat: add <Stack> ... </Stack> (#132)

---
+++ src/components/Stack/Stack.types.ts
@@ -0,0 +1,72 @@
+import { OcBaseProps } from '../OcBase';
+
+export type StackDirection = 'vertical' | 'horizontal';
+
+export type StackGap =
+    | 'xxxs'
+    | 'xxs'
+    | 'xs'
+    | 's'
+    | 'm'
+    | 'l'
+    | 'xl'
+    | 'xxl'
+    | 'xxl'
+    | 'xxxl';
+
+type StackIntrinsicProps = {
+    /**
+     * Direction type - horizontal or vertical
+     */
+    direction?: StackDirection;
+
+    /**
+     * Space between the child elements
+     */
+    gap?: StackGap;
+
+    /**
+     * Wrap behaviour for the stack
+     */
+    wrap?: 'wrap' | 'nowrap' | 'wrap-reverse';
+
+    /**
+     * Enable stack as an inline element
+     */
+    inline?: boolean;
+
+    /**
+     * Assigns the stack 100% width
+     */
+    fullWidth?: boolean;
+
+    /**
+     * Align Items
+     */
+    align?:
+        | 'stretch'
+        | 'center'
+        | 'flex-start'
+        | 'flex-end'
+        | 'baseline'
+        | 'initial'
+        | 'inherit';
+
+    /**
+     * Justify Content
+     */
+    justify?:
+        | 'flex-start'
+        | 'flex-end'
+        | 'center'
+        | 'space-between'
+        | 'space-around'
+        | 'space-evenly';
+};
+
+export type StackBreakpoint = 'xsmall' | 'small' | 'medium' | 'large';
+export interface StackProps
+    extends StackIntrinsicProps,
+        OcBaseProps<HTMLDivElement> {
+    breakpoints?: Partial<Record<StackBreakpoint, StackIntrinsicProps>>;
+}
