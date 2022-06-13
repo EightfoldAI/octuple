515fdeb8a54c7321093a4d4ce326ced738b26513
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2022-06-13 11:01:23 -0700

    fix: inputs: adds forward ref support, adjusts default wait interval (#177)

--- src/components/Inputs/TextArea/TextArea.tsx
+++ src/components/Inputs/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, Ref, useState } from 'react';
 import { Icon, IconName } from '../../Icon';
 import { Label } from '../../Label';
 import { TextInputWidth, TextAreaProps, TextInputTheme } from '../index';
@@ -7,94 +7,105 @@ import { mergeClasses, uniqueId } from '../../../shared/utilities';
 
 import styles from '../input.module.scss';
 
-export const TextArea: FC<TextAreaProps> = ({
-    allowDisabledFocus = false,
-    ariaLabel,
-    autoFocus = false,
-    classNames,
-    disabled = false,
-    enableExpand = false,
-    id,
-    inputWidth = TextInputWidth.fitContent,
-    labelProps,
-    maxlength,
-    minlength,
-    name,
-    onBlur,
-    onChange,
-    onFocus,
-    onKeyDown,
-    placeholder,
-    required = false,
-    style,
-    textAreaCols = 50,
-    textAreaRows = 5,
-    theme = TextInputTheme.light,
-    value,
-    waitInterval = 10,
-    ...rest
-}) => {
-    const [textAreaId] = useState<string>(uniqueId(id || 'textarea-'));
-
-    const textAreaClassNames: string = mergeClasses([
-        classNames,
-        styles.textArea,
-        { [styles.textAreaNoExpand]: !enableExpand },
-        { [styles.dark]: theme === TextInputTheme.dark },
-        { [styles.inputStretch]: inputWidth === TextInputWidth.fill },
-    ]);
-
-    const textAreaWrapperClassNames: string = mergeClasses([
-        styles.inputWrapper,
+export const TextArea: FC<TextAreaProps> = React.forwardRef(
+    (
         {
-            [styles.inputStretch]: inputWidth === TextInputWidth.fill,
+            allowDisabledFocus = false,
+            ariaLabel,
+            autoFocus = false,
+            classNames,
+            disabled = false,
+            enableExpand = false,
+            id,
+            inputWidth = TextInputWidth.fitContent,
+            labelProps,
+            maxlength,
+            minlength,
+            name,
+            onBlur,
+            onChange,
+            onFocus,
+            onKeyDown,
+            placeholder,
+            required = false,
+            style,
+            textAreaCols = 50,
+            textAreaRows = 5,
+            theme = TextInputTheme.light,
+            value,
+            waitInterval = 10,
+            ...rest
         },
-    ]);
-
-    const handleChange = useDebounce<React.ChangeEvent<HTMLTextAreaElement>>(
-        (_event?: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) =>
-            triggerChange(_event),
-        waitInterval
-    );
-
-    const triggerChange = (
-        _event?: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+        ref: Ref<HTMLTextAreaElement>
     ) => {
-        onChange && onChange(_event);
-    };
+        const [textAreaId] = useState<string>(uniqueId(id || 'textarea-'));
 
-    return (
-        <div className={textAreaWrapperClassNames}>
-            {labelProps && <Label {...labelProps} />}
-            <textarea
-                {...rest}
-                aria-disabled={allowDisabledFocus}
-                aria-label={ariaLabel}
-                autoFocus={autoFocus}
-                className={textAreaClassNames}
-                cols={textAreaCols}
-                disabled={disabled}
-                id={textAreaId}
-                maxLength={maxlength}
-                minLength={minlength}
-                name={name}
-                onChange={!allowDisabledFocus ? handleChange : null}
-                onBlur={!allowDisabledFocus ? onBlur : null}
-                onFocus={!allowDisabledFocus ? onFocus : null}
-                onKeyDown={!allowDisabledFocus ? onKeyDown : null}
-                placeholder={placeholder}
-                required={required}
-                style={style}
-                rows={textAreaRows}
-                tabIndex={0}
-                value={value}
-            />
-            {enableExpand && (
-                <Icon
-                    classNames={styles.textAreaResizeIcon}
-                    path={IconName.mdiResizeBottomRight}
+        const textAreaClassNames: string = mergeClasses([
+            classNames,
+            styles.textArea,
+            { [styles.textAreaNoExpand]: !enableExpand },
+            { [styles.dark]: theme === TextInputTheme.dark },
+            { [styles.inputStretch]: inputWidth === TextInputWidth.fill },
+        ]);
+
+        const textAreaWrapperClassNames: string = mergeClasses([
+            styles.inputWrapper,
+            {
+                [styles.inputStretch]: inputWidth === TextInputWidth.fill,
+            },
+        ]);
+
+        const handleChange = useDebounce<
+            React.ChangeEvent<HTMLTextAreaElement>
+        >(
+            (
+                _event?: React.ChangeEvent<
+                    HTMLTextAreaElement | HTMLInputElement
+                >
+            ) => triggerChange(_event),
+            waitInterval
+        );
+
+        const triggerChange = (
+            _event?: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+        ) => {
+            onChange && onChange(_event);
+        };
+
+        return (
+            <div className={textAreaWrapperClassNames}>
+                {labelProps && <Label {...labelProps} />}
+                <textarea
+                    {...rest}
+                    ref={ref}
+                    aria-disabled={allowDisabledFocus}
+                    aria-label={ariaLabel}
+                    autoFocus={autoFocus}
+                    className={textAreaClassNames}
+                    cols={textAreaCols}
+                    disabled={disabled}
+                    id={textAreaId}
+                    maxLength={maxlength}
+                    minLength={minlength}
+                    name={name}
+                    onChange={!allowDisabledFocus ? handleChange : null}
+                    onBlur={!allowDisabledFocus ? onBlur : null}
+                    onFocus={!allowDisabledFocus ? onFocus : null}
+                    onKeyDown={!allowDisabledFocus ? onKeyDown : null}
+                    placeholder={placeholder}
+                    required={required}
+                    style={style}
+                    rows={textAreaRows}
+                    tabIndex={0}
+                    value={value}
                 />
-            )}
-        </div>
-    );
-};
+                {enableExpand && (
+                    <Icon
+                        classNames={styles.textAreaResizeIcon}
+                        path={IconName.mdiResizeBottomRight}
+                    />
+                )}
+            </div>
+        );
+    }
+);
