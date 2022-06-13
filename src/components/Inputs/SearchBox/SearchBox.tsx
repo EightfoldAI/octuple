996f8d62f8dd28030f5fd41ce81434e24c6ae3a9
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2022-06-13 11:01:23 -0700

    fix: inputs: adds forward ref support, adjusts default wait interval (#177)

--- src/components/Inputs/SearchBox/SearchBox.tsx
+++ src/components/Inputs/SearchBox/SearchBox.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, Ref } from 'react';
 import { IconName } from '../../Icon';
 import {
     TextInputWidth,
@@ -8,63 +8,71 @@ import {
     TextInputTheme,
 } from '../index';
 
-export const SearchBox: FC<SearchBoxProps> = ({
-    allowDisabledFocus = false,
-    ariaLabel,
-    autoFocus = false,
-    classNames,
-    clearButtonAriaLabel,
-    disabled = false,
-    iconProps,
-    iconButtonProps = {
-        allowDisabledFocus: false,
-        disabled: false,
-        iconProps: { path: IconName.mdiMagnify },
-    },
-    inputWidth = TextInputWidth.fitContent,
-    labelProps,
-    maxlength,
-    minlength,
-    name,
-    onBlur,
-    onChange,
-    onFocus,
-    onKeyDown,
-    placeholder = 'Search',
-    shape = TextInputShape.Rectangle,
-    style,
-    theme = TextInputTheme.light,
-    value,
-    waitInterval = 500,
-    ...rest
-}) => (
-    <form role="search">
-        <TextInput
-            {...rest}
-            allowDisabledFocus={allowDisabledFocus}
-            ariaLabel={ariaLabel}
-            autoFocus={autoFocus}
-            classNames={classNames}
-            clearButtonAriaLabel={clearButtonAriaLabel}
-            disabled={disabled}
-            htmlType="search"
-            iconProps={iconProps}
-            iconButtonProps={iconButtonProps}
-            inputWidth={inputWidth}
-            labelProps={labelProps}
-            maxlength={maxlength}
-            minlength={minlength}
-            name={name}
-            onBlur={onBlur}
-            onChange={onChange}
-            onFocus={onFocus}
-            onKeyDown={onKeyDown}
-            placeholder={placeholder}
-            shape={shape}
-            style={style}
-            theme={theme}
-            value={value}
-            waitInterval={waitInterval}
-        />
-    </form>
+export const SearchBox: FC<SearchBoxProps> = React.forwardRef(
+    (
+        {
+            allowDisabledFocus = false,
+            ariaLabel,
+            autoFocus = false,
+            classNames,
+            clearButtonAriaLabel,
+            disabled = false,
+            iconProps,
+            iconButtonProps = {
+                allowDisabledFocus: false,
+                disabled: false,
+                iconProps: { path: IconName.mdiMagnify },
+            },
+            inputWidth = TextInputWidth.fitContent,
+            labelProps,
+            maxlength,
+            minlength,
+            name,
+            onBlur,
+            onChange,
+            onFocus,
+            onKeyDown,
+            placeholder = 'Search',
+            shape = TextInputShape.Rectangle,
+            style,
+            theme = TextInputTheme.light,
+            value,
+            waitInterval = 500,
+            ...rest
+        },
+        ref: Ref<HTMLInputElement>
+    ) => {
+        return (
+            <form role="search">
+                <TextInput
+                    {...rest}
+                    ref={ref}
+                    allowDisabledFocus={allowDisabledFocus}
+                    ariaLabel={ariaLabel}
+                    autoFocus={autoFocus}
+                    classNames={classNames}
+                    clearButtonAriaLabel={clearButtonAriaLabel}
+                    disabled={disabled}
+                    htmlType="search"
+                    iconProps={iconProps}
+                    iconButtonProps={iconButtonProps}
+                    inputWidth={inputWidth}
+                    labelProps={labelProps}
+                    maxlength={maxlength}
+                    minlength={minlength}
+                    name={name}
+                    onBlur={onBlur}
+                    onChange={onChange}
+                    onFocus={onFocus}
+                    onKeyDown={onKeyDown}
+                    placeholder={placeholder}
+                    shape={shape}
+                    style={style}
+                    theme={theme}
+                    value={value}
+                    waitInterval={waitInterval}
+                />
+            </form>
+        );
+    }
 );
