9927e96641c014159c41c161da8d33707675cddb
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2022-06-13 22:33:54 -0700

    feat: empty: adds empty state component (#180)
    * feat: empty: adds empty state component
    
    * chore: empty: updates tests

---
+++ src/components/Empty/Empty.stories.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Stories } from '@storybook/addon-docs';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Empty, EmptyMode } from './';
+import { Icon, IconName } from '../Icon';
+
+export default {
+    title: 'Empty',
+    parameters: {
+        docs: {
+            page: (): JSX.Element => (
+                <main>
+                    <article>
+                        <section>
+                            <h1>Empty</h1>
+                            <p>An Empty state placeholder.</p>
+                        </section>
+                        <section>
+                            <Stories includePrimary title="" />
+                        </section>
+                    </article>
+                </main>
+            ),
+        },
+    },
+    argTypes: {
+        mode: {
+            options: [
+                EmptyMode.data,
+                EmptyMode.error,
+                EmptyMode.messages,
+                EmptyMode.search,
+                EmptyMode.tasks,
+            ],
+            control: { type: 'select' },
+        },
+    },
+} as ComponentMeta<typeof Empty>;
+
+const Empty_Story: ComponentStory<typeof Empty> = (args) => <Empty {...args} />;
+
+export const No_Data = Empty_Story.bind({});
+export const Error_State = Empty_Story.bind({});
+export const Empty_Messages = Empty_Story.bind({});
+export const No_Search_Results = Empty_Story.bind({});
+export const Tasks_Complete = Empty_Story.bind({});
+export const Custom_Image = Empty_Story.bind({});
+
+const emptyArgs: Object = {
+    description: 'More detail on how might the user be able to get around this',
+    mode: EmptyMode.data,
+    style: {},
+    title: 'Short Message Here',
+};
+
+No_Data.args = {
+    ...emptyArgs,
+};
+
+Error_State.args = {
+    ...emptyArgs,
+    mode: EmptyMode.error,
+};
+
+Empty_Messages.args = {
+    ...emptyArgs,
+    mode: EmptyMode.messages,
+};
+
+No_Search_Results.args = {
+    ...emptyArgs,
+    mode: EmptyMode.search,
+};
+
+Tasks_Complete.args = {
+    ...emptyArgs,
+    mode: EmptyMode.tasks,
+};
+
+Custom_Image.args = {
+    ...emptyArgs,
+    image: (
+        <Icon color="#8ED0FA" path={IconName.mdiWrenchOutline} size="200px" />
+    ),
+    imageStyle: {},
+    mode: '',
+};
---
+++ src/components/Empty/Empty.tsx
@@ -0,0 +1,88 @@
+import React, { FC, Ref } from 'react';
+import { mergeClasses } from '../../shared/utilities';
+import { EmptyMode, EmptyProps } from './Empty.types';
+import { DefaultEmptyDataImg } from './SVG/DefaultEmptyDataImg';
+import { DefaultEmptyMessagesImg } from './SVG/DefaultEmptyMessagesImg';
+import { DefaultEmptySearchImg } from './SVG/DefaultEmptySearchImg';
+import { DefaultServerErrorImg } from './SVG/DefaultServerErrorImg';
+import { DefaultTasksCompleteImg } from './SVG/DefaultTasksCompleteImg';
+import { useCanvasDirection } from '../../hooks/useCanvasDirection';
+
+import styles from './empty.module.scss';
+
+const defaultEmptyDataImg = <DefaultEmptyDataImg />;
+const defaultEmptyMessagesImg = <DefaultEmptyMessagesImg />;
+const defaultEmptySearchImg = <DefaultEmptySearchImg />;
+const defaultServerErrorImg = <DefaultServerErrorImg />;
+const defaultTasksCompleteImg = <DefaultTasksCompleteImg />;
+
+export const Empty: FC<EmptyProps> = React.forwardRef(
+    (
+        {
+            children,
+            classNames,
+            description,
+            image,
+            imageStyle,
+            mode = EmptyMode.data,
+            title,
+            ...rest
+        },
+        ref: Ref<HTMLDivElement>
+    ) => {
+        const htmlDir: string = useCanvasDirection();
+
+        const getDefaultImage = (mode: EmptyMode): JSX.Element => {
+            switch (mode) {
+                case EmptyMode.data:
+                    return defaultEmptyDataImg;
+                case EmptyMode.error:
+                    return defaultServerErrorImg;
+                case EmptyMode.messages:
+                    return defaultEmptyMessagesImg;
+                case EmptyMode.search:
+                    return defaultEmptySearchImg;
+                case EmptyMode.tasks:
+                    return defaultTasksCompleteImg;
+                default:
+                    return defaultEmptyDataImg;
+            }
+        };
+
+        let imageNode: React.ReactNode = null;
+
+        if (image) {
+            if (typeof image === 'string') {
+                imageNode = <img alt={description} src={image} />;
+            } else {
+                imageNode = image;
+            }
+        } else {
+            imageNode = getDefaultImage(mode);
+        }
+
+        return (
+            <div
+                {...rest}
+                ref={ref}
+                className={mergeClasses([
+                    styles.empty,
+                    { [styles.emptyDefault]: image === getDefaultImage(mode) },
+                    { [styles.emptyRtl]: htmlDir === 'rtl' },
+                    classNames,
+                ])}
+            >
+                <div className={styles.emptyImage} style={imageStyle}>
+                    {imageNode}
+                </div>
+                {title && <div className={styles.emptyTitle}>{title}</div>}
+                {description && (
+                    <div className={styles.emptyDescription}>{description}</div>
+                )}
+                {children && (
+                    <div className={styles.emptyFooter}>{children}</div>
+                )}
+            </div>
+        );
+    }
+);
