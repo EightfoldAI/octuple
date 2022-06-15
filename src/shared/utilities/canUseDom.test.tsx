c528ed4ab0d31e20e3c2c79b351c306006cb0972
Jeff Howard <106610186+jhoward-eightfold@users.noreply.github.com>
2022-06-15 13:02:30 -0500

    chore: rename hasWindow and update its logic

--- src/shared/utilities/canUseDom.test.tsx
+++ src/shared/utilities/canUseDom.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { hasWindow } from './hasWindow';
+import { canUseDom } from './canUseDom';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('hasWindow', () => {
+describe('canUseDom', () => {
     test('should return true when the function is called in the browser', () => {
-        expect(hasWindow()).toBe(true);
+        expect(canUseDom()).toBe(true);
     });
 });
--- src/shared/utilities/hasWindow.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Utility to verify if the window object exists
- * @returns {boolean}
- */
-export const hasWindow = (): boolean => {
-    return typeof window !== 'undefined';
-};
