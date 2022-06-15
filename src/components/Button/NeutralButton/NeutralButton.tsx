45eaec1f04d9a8bf212bd0c776c50e5bfbfaab74
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2022-06-15 10:24:37 -0700

    feat: button: adds counter badge support to all buttons (#187)
    * feat: button: adds counter badge support to all buttons
    
    * chore: button: address counter pr feedback

--- src/components/Button/NeutralButton/NeutralButton.tsx
+++ src/components/Button/NeutralButton/NeutralButton.tsx
@@ -21,6 +21,7 @@ export const NeutralButton: FC<ButtonProps> = React.forwardRef(
             ariaLabel,
             checked = false,
             classNames,
+            counter,
             disabled = false,
             dropShadow = false,
             htmlType,
@@ -56,6 +57,7 @@ export const NeutralButton: FC<ButtonProps> = React.forwardRef(
                 ariaLabel={ariaLabel}
                 checked={checked}
                 classNames={buttonClassNames}
+                counter={counter}
                 disabled={disabled}
                 dropShadow={dropShadow}
                 htmlType={htmlType}
