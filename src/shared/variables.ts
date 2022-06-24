bf6b4b443f6ea8443a90ea6d2db1fb66111a7a2f
Dylan Kilgore <dkilgore@eightfold.ai>
2022-06-23 21:08:39 -0700

    chore: button: remove dupe

--- src/shared/variables.ts
+++ src/shared/variables.ts
@@ -166,7 +166,6 @@ export interface CSSVariables extends React.CSSProperties {
 
     '--button-default-outline-color'?: string;
     '--button-hover-outline-color'?: string;
-    '--button-focus-outline-color'?: string;
     '--button-active-outline-color'?: string;
     '--button-visited-outline-color'?: string;
 
