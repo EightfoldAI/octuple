0a86e9725ed89e61ad41b0aad5fb094b992986ed
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2022-06-29 12:35:39 -0700

    chore: tidy: renames types and fix console warnings in svg components (#211)

--- src/components/Empty/SVG/DefaultServerErrorImg.tsx
+++ src/components/Empty/SVG/DefaultServerErrorImg.tsx
@@ -18,7 +18,7 @@ export const DefaultServerErrorImg = (): JSX.Element => {
                 d="M49.2784 101.405C39.3247 57.4604 105.676 18.8428 136.863 78.7671C196.578 76.1036 191.934 156.002 145.489 158L107.668 158L46.6243 158C12.7837 158 0.841885 104.068 49.2784 101.405Z"
                 fill="white"
                 stroke="#8ED0FA"
-                stroke-width="6"
+                strokeWidth="6"
             />
             <path
                 d="M76.0193 103.097L82.4167 109.495L86.6594 105.252L80.2619 98.8548L76.0193 103.097ZM82.4167 108.081L76.38 114.117L80.6226 118.36L86.6594 112.323L82.4167 108.081ZM82.4167 109.495C82.0262 109.104 82.0262 108.471 82.4167 108.081L86.6594 112.323C88.612 110.371 88.612 107.205 86.6594 105.252L82.4167 109.495Z"
@@ -32,17 +32,17 @@ export const DefaultServerErrorImg = (): JSX.Element => {
                 d="M102 142C107.192 142 111 137.516 111 132.5C111 127.484 107.192 123 102 123C96.8079 123 93 127.484 93 132.5C93 137.516 96.8079 142 102 142Z"
                 fill="#B0F3FE"
                 stroke="#B0F3FE"
-                stroke-width="6"
+                strokeWidth="6"
             />
             <path
                 d="M161 68C166.192 68 170 63.5161 170 58.5C170 53.4839 166.192 49 161 49C155.808 49 152 53.4839 152 58.5C152 63.5161 155.808 68 161 68Z"
                 stroke="#B0F3FE"
-                stroke-width="6"
+                strokeWidth="6"
             />
             <path
                 d="M55 22C60.1921 22 64 17.5161 64 12.5C64 7.48391 60.1921 3 55 3C49.8079 3 46 7.48391 46 12.5C46 17.5161 49.8079 22 55 22Z"
                 stroke="#B0F3FE"
-                stroke-width="6"
+                strokeWidth="6"
             />
             <ellipse cx="108" cy="29.5" rx="6" ry="6.5" fill="#B0F3FE" />
             <ellipse cx="28" cy="89.5" rx="6" ry="6.5" fill="#B0F3FE" />
@@ -52,7 +52,7 @@ export const DefaultServerErrorImg = (): JSX.Element => {
                 x2="148.606"
                 y2="24.3346"
                 stroke="#B0F3FE"
-                stroke-width="6"
+                strokeWidth="6"
                 strokeLinecap="round"
             />
             <line
@@ -61,7 +61,7 @@ export const DefaultServerErrorImg = (): JSX.Element => {
                 x2="148.608"
                 y2="11.6065"
                 stroke="#B0F3FE"
-                stroke-width="6"
+                strokeWidth="6"
                 strokeLinecap="round"
             />
             <line
@@ -70,7 +70,7 @@ export const DefaultServerErrorImg = (): JSX.Element => {
                 x2="18.6061"
                 y2="51.3346"
                 stroke="#B0F3FE"
-                stroke-width="6"
+                strokeWidth="6"
                 strokeLinecap="round"
             />
             <line
@@ -79,7 +79,7 @@ export const DefaultServerErrorImg = (): JSX.Element => {
                 x2="18.6076"
                 y2="38.6065"
                 stroke="#B0F3FE"
-                stroke-width="6"
+                strokeWidth="6"
                 strokeLinecap="round"
             />
         </svg>
