a36bcf0ea35682b1dd2c395aa51bf830d69d6a46
Yash Raj Chhabra <ychhabra@eightfold.ai>
2022-07-19 15:51:28 +0530

    chore: custom menu item styling

--- src/components/Menu/MenuItem/MenuItemCustom/MenuItemCustom.tsx
+++ src/components/Menu/MenuItem/MenuItemCustom/MenuItemCustom.tsx
@@ -1,15 +1,27 @@
 import React, { FC } from 'react';
 import { MenuItemCustomProps } from '../MenuItem.types';
+import { mergeClasses } from '../../../../shared/utilities';
+import { MenuSize } from '../../Menu.types';
 
-import styles from '../../menu.module.scss';
+import styles from '../menuItem.module.scss';
 
 export const MenuItemCustom: FC<MenuItemCustomProps> = ({
     index,
     onChange,
+    size,
     ...item
 }) => {
+    const menuItemClasses = mergeClasses([
+        styles.menuItemCustom,
+        {
+            [styles.large]: size === MenuSize.large,
+            [styles.medium]: size === MenuSize.medium,
+            [styles.small]: size === MenuSize.small,
+        },
+    ]);
+
     return (
-        <div role="menuitem" className={styles.menuItemCustom}>
+        <div className={menuItemClasses}>
             {item.render({ index, value: item, onChange })}
         </div>
     );
