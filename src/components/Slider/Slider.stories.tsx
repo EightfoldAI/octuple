4476c86f2353b69ea201c671f369a4a504e6ebe7
Dylan Kilgore <dkilgore@eightfold.ai>
2022-07-28 10:44:10 -0700

    fix: slider: fixes slider onchange event hang in react 16
    adds some typings

--- src/components/Slider/Slider.stories.tsx
+++ src/components/Slider/Slider.stories.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Stack } from '../Stack';
 
 import { Slider } from './';
 
@@ -8,9 +9,67 @@ export default {
     component: Slider,
 } as ComponentMeta<typeof Slider>;
 
-const Slider_Story: ComponentStory<typeof Slider> = (args) => (
-    <Slider {...args} />
-);
+const Slider_Story: ComponentStory<typeof Slider> = (args) => {
+    const [transientSlidingValue, setTransientSlidingValue] =
+        useState<number>(2);
+
+    const handleChange = (val: number): void => {
+        setTransientSlidingValue(val);
+    };
+
+    return (
+        <Stack direction="vertical" gap="xl" style={{ width: '100%' }}>
+            <Slider
+                {...args}
+                value={transientSlidingValue}
+                onChange={handleChange}
+            />
+            <Stack
+                direction="horizontal"
+                gap="xl"
+                justify="center"
+                style={{ width: '100%' }}
+            >
+                <div>{transientSlidingValue}</div>
+            </Stack>
+        </Stack>
+    );
+};
+
+const Range_Slider_Story: ComponentStory<typeof Slider> = (args) => {
+    const [transientSlidingValues, setTransientSlidingValues] = useState<
+        number[]
+    >([110, 150]);
+
+    const handleChange = (vals: number[]): void => {
+        setTransientSlidingValues(vals);
+    };
+
+    return (
+        <Stack
+            align="stretch"
+            direction="vertical"
+            gap="xl"
+            justify="center"
+            style={{ width: '100%' }}
+        >
+            <Slider
+                {...args}
+                value={transientSlidingValues}
+                onChange={handleChange}
+            />
+            <Stack
+                direction="horizontal"
+                gap="xl"
+                justify="center"
+                style={{ width: '100%' }}
+            >
+                <div>{transientSlidingValues[0]}</div>
+                <div>{transientSlidingValues[1]}</div>
+            </Stack>
+        </Stack>
+    );
+};
 
 const sliderArgs: Object = {
     ariaLabel: 'Slider',
@@ -22,7 +81,7 @@ const sliderArgs: Object = {
     max: 200,
     name: 'mySlider',
     onChange: () => {
-        // handle change.
+        console.log('changed');
     },
 };
 
@@ -32,11 +91,12 @@ StandardSlider.args = {
     autoFocus: true,
     min: 1,
     max: 5,
-    showLabels: false,
+    showLabels: true,
+    showMarkers: true,
     value: 2,
 };
 
-export const RangeSlider = Slider_Story.bind({});
+export const RangeSlider = Range_Slider_Story.bind({});
 RangeSlider.args = {
     ...sliderArgs,
     min: 0,
