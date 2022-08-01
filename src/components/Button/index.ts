36d516e94f66d510ceaeb98372f8b2285e1c5363
Dylan Kilgore <dkilgore@eightfold.ai>
2022-08-01 13:32:04 -0700

    feat: system ui button: add system ui button

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -5,4 +5,5 @@ export * from './NeutralButton/NeutralButton';
 export * from './PrimaryButton/PrimaryButton';
 export * from './SecondaryButton/SecondaryButton';
 export * from './SplitButton/SplitButton';
+export * from './SystemUIButton/SystemUIButton';
 export * from './TwoStateButton/TwoStateButton';
