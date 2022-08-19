8f839db3974253fada83b13573a1f32f3c2c51c6
fazil-eigthfold <mfazil@eigthfold.ai>
2022-08-19 18:37:23 +0530

    chore: added default value

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -209,6 +209,7 @@ export interface ButtonProps extends NativeButtonProps {
     toggle?: boolean;
     /**
      * The button will remain transparent
+     * @default false
      */
     transparent?: boolean;
 }
