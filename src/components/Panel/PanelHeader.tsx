6e80dade501df9a48178551e618e7050b6f35827
mfazil-eightfold <109717425+mfazil-eightfold@users.noreply.github.com>
2022-08-24 12:40:31 +0530

    feat: panel custom header (#318)
    * feat: adds headerbuttonprops, headericon, and overlay props to surface ui
    
    * chore: initial design of panel header
    
    * chore: removed unwated object properties
    
    * chore: updated close icon color
    
    * chore: pr ready
    
    * chore:pr feedback changes
    
    * chore: more pr feedback changes
    
    * chore: pr feedback changes
    
    * chore: new changes to accomodate action buttons
    
    * chore: removed strict type conversion (!!)
    
    * chore: test added
    
    * fix: test changes
    
    Co-authored-by: fazil-eigthfold <mfazil@eigthfold.ai>

---
+++ src/components/Panel/PanelHeader.tsx
@@ -0,0 +1,47 @@
+import React, { FC } from 'react';
+
+import { PanelHeaderProps } from './Panel.types';
+
+import { SystemUIButton } from '../Button';
+import { IconName } from '../Icon';
+
+import styles from './panel.module.scss';
+
+export const PanelHeader: FC<PanelHeaderProps> = ({
+    onClose,
+    closeIcon = IconName.mdiClose,
+    title,
+    actionDefaultButtonProps,
+    actionButtonOneProps,
+    actionButtonTwoProps,
+}) => {
+    return (
+        <div className={styles.logoGradientHeaderWrapper}>
+            <div className={styles.headerTitle}>
+                {actionDefaultButtonProps && (
+                    <SystemUIButton {...actionDefaultButtonProps} />
+                )}
+                {title}
+            </div>
+            <div className={styles.headerActionButtons}>
+                {actionButtonOneProps && (
+                    <SystemUIButton {...actionButtonOneProps} />
+                )}
+                {actionButtonTwoProps && (
+                    <SystemUIButton {...actionButtonTwoProps} />
+                )}
+                {onClose && (
+                    <SystemUIButton
+                        iconProps={{
+                            path: closeIcon,
+                            color: 'var(--white-color)',
+                        }}
+                        ariaLabel={'Close'}
+                        onClick={onClose}
+                        transparent
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
