a521d9c3173518bf6d6b63fb534c819bb82d0938
Dylan Kilgore <dkilgore@eightfold.ai>
2022-08-30 18:42:10 -0700

    chore: fixes spinner typing

--- src/components/Spinner/Spinner.types.ts
+++ src/components/Spinner/Spinner.types.ts
@@ -1,12 +1,18 @@
-import { OcBaseProps } from '../OcBase';
-
 export enum SpinnerSize {
     Large = '64px',
     Default = '48px',
     Small = '30px',
 }
 
-export interface SpinnerProps extends OcBaseProps<HTMLElement> {
+export interface SpinnerProps extends React.HTMLAttributes<HTMLElement> {
+    /**
+     * Custom classnames of the component
+     */
+    classNames?: string;
+    /**
+     * Unique id used to target element for testing
+     */
+    'data-test-id'?: string;
     /**
      * Size of the spinner
      * @default SpinnerSize.Default
