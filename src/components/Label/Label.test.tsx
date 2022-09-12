3037beb71b7c8eb70e56555986301d5b125d57a6
Dylan Kilgore <dkilgore@eightfold.ai>
2022-09-12 09:38:56 -0700

    chore: adds unit tests and updates snaps

--- src/components/Label/Label.test.tsx
+++ src/components/Label/Label.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { Label } from './index';
+import { ButtonShape } from '../Button';
 import MatchMediaMock from 'jest-matchmedia-mock';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -19,6 +20,39 @@ describe('Label', () => {
 
     test('label renders', () => {
         const wrapper = mount(<Label text="Test label" />);
+        expect(wrapper.containsMatchingElement(<Label />)).toEqual(true);
+    });
+    test('label renders with default button', () => {
+        const wrapper = mount(
+            <Label
+                labelIconButtonProps={{
+                    show: true,
+                    toolTipContent: 'A tooltip',
+                    toolTipPlacement: 'right',
+                }}
+                text="Test label"
+            />
+        );
+        expect(wrapper.containsMatchingElement(<Label />)).toEqual(true);
+    });
+    test('label renders with custom button', () => {
+        const wrapper = mount(
+            <Label
+                labelIconButtonProps={{
+                    iconProps: {
+                        path: null,
+                    },
+                    shape: ButtonShape.Pill,
+                    show: true,
+                    style: {
+                        marginLeft: 8,
+                        marginTop: -8,
+                    },
+                    text: 'Learn more',
+                }}
+                text="Test label"
+            />
+        );
         expect(wrapper.containsMatchingElement(<Label />)).toEqual(true);
     });
 });
