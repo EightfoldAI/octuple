532c0910c5902a5912091c2dfe00cb89a72bbeeb
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2022-10-12 14:16:21 -0700

    fix: select: adds html type to chevron button (#419)

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -479,6 +479,7 @@ export const Select: FC<SelectProps> = React.forwardRef(
             clearable: clearable,
             inputWidth: inputWidth,
             iconButtonProps: {
+                htmlType: 'button',
                 iconProps: {
                     path: dropdownVisible
                         ? IconName.mdiChevronUp
