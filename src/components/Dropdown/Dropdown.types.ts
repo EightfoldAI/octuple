106034f46c643d77a92896546ef1dea00ac9da6f
Yash Raj Chhabra <ychhabra@eightfold.ai>
2022-10-13 17:09:44 +0530

    fix: dropdown: remove inputRef from dropdown and use update method to fix position

--- src/components/Dropdown/Dropdown.types.ts
+++ src/components/Dropdown/Dropdown.types.ts
@@ -1,4 +1,4 @@
-import React, { MutableRefObject } from 'react';
+import React, { Ref } from 'react';
 import { Placement, Strategy } from '@floating-ui/react-dom';
 
 export interface DropdownProps {
@@ -32,10 +32,6 @@ export interface DropdownProps {
      * Manually control the height of the dropdown
      */
     height?: number;
-    /**
-     * Include dropdown textbox ref if using Select component
-     */
-    inputRef?: MutableRefObject<HTMLInputElement>;
     /**
      * The offset from the reference element
      * @default 0
@@ -94,4 +90,16 @@ export interface DropdownProps {
      * Manually control the width of the dropdown
      */
     width?: number;
+    /**
+     * The ref of the dropdown
+     */
+    ref?: Ref<DropdownRef>;
 }
+
+export type DropdownRef = {
+    /**
+     * Helper method to manually update the position
+     * of the dropdown
+     */
+    update: () => void;
+};
