1f0ddfb10646c8bbd2ef99174f3ede74832033a2
Dylan <99700808+dkilgore-eightfold@users.noreply.github.com>
2023-01-03 16:57:27 -0800

    fix: upload: fixes rendering error caused by hooks in class component uid utility regression (#488)
    also fixes pill tabs badge default bg color

--- src/components/Upload/Internal/uid.ts
+++ src/components/Upload/Internal/uid.ts
@@ -1,6 +1,13 @@
-import { uniqueId } from '../../../shared/utilities';
-
 const now = +new Date();
+
+/**
+ * Generates a simple unique id to be used by Upload class component
+ * @param prefix The sting prefix
+ */
+export const uniqueId = ((): ((prefix: string) => string) => {
+  let counter: number = 0;
+  return (prefix: string): string => `${prefix}${++counter}`;
+})();
 
 export const uid = (): string => {
   return uniqueId(`upload-${now}`);
