078cc7aebf2de057e7855f332d9abbe6d078aa5f
Yash Raj Chhabra <ychhabra@eightfold.ai>
2023-01-30 23:16:23 +0530

    fix: slider: update state when value changes externally

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -4,6 +4,7 @@ import React, {
   Ref,
   RefObject,
   useContext,
+  useEffect,
   useLayoutEffect,
   useMemo,
   useRef,
@@ -129,8 +130,15 @@ export const Slider: FC<SliderProps> = React.forwardRef(
 
     const isRange: boolean = Array.isArray(value);
     const [values, setValues] = useState<number[]>(
-      Array.isArray(value) ? value.sort(asc) : [value]
+      isRange ? value.sort(asc) : [value]
     );
+
+    useEffect(() => {
+      if (value !== values) {
+        setValues(Array.isArray(value) ? value.sort(asc) : [value]);
+      }
+    }, [value]);
+
     const sliderRef: React.MutableRefObject<HTMLDivElement> =
       useRef<HTMLDivElement>(null);
     const railRef: React.MutableRefObject<HTMLDivElement> =
