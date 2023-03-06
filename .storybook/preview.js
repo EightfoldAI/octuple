f369876a3e67300d35917d395c2da98418cd9e7f
Yash Raj Chhabra <ychhabra@eightfold.ai>
2023-03-06 16:24:17 +0530

    fix: accordion and shimmer

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,2 +1,15 @@
 import { initializeRTL } from 'storybook-addon-rtl';
+import { themes } from '@storybook/theming';
 initializeRTL();
+
+export const parameters = {
+  darkMode: {
+    // Override the default dark theme
+    dark: {
+      ...themes.dark,
+      appBg: '#1a212e',
+      appContentBg: '#1a212e',
+      barBg: '#1a212e',
+    },
+  },
+};
