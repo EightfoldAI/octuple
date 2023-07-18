385e107e9f33e356c81c509ed84813ccf68d0d14
Dylan Kilgore <dkilgore@eightfold.ai>
2023-07-18 14:59:33 -0700

    chore: coverage: adds unit tests to components that need them

---
+++ src/components/Skeleton/Skeleton.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import MatchMediaMock from 'jest-matchmedia-mock';
+import { Button } from '../Button';
+import { Skeleton } from './Skeleton';
+import { SkeletonAnimation, SkeletonProps } from './Skeleton.types';
+import { render } from '@testing-library/react';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+let matchMedia: any;
+
+const skeletonProps: SkeletonProps = {
+  animating: true,
+  animation: SkeletonAnimation.Wave,
+};
+
+describe('Skeleton', () => {
+  beforeAll(() => {
+    matchMedia = new MatchMediaMock();
+  });
+
+  afterEach(() => {
+    matchMedia.clear();
+  });
+
+  test('Renders without crashing', () => {
+    const { container } = render(
+      <Skeleton {...skeletonProps}>
+        <Button text={'Sample button'} />
+      </Skeleton>
+    );
+    const skeleton = container.getElementsByClassName('skeleton');
+    expect(() => container).not.toThrowError();
+    expect(skeleton).toBeTruthy();
+    expect(container).toMatchSnapshot();
+  });
+});
