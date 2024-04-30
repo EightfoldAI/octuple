fd3c223900177d08e2a81aa78e50544f1afa4ca9
Grant Clark <gclark@eightfold.ai>
2024-04-29 20:06:50 -0700

    feat: export ParentComponentContext

--- src/components/ConfigProvider/ParentComponentsContext.tsx
+++ src/components/ConfigProvider/ParentComponentsContext.tsx
@@ -2,13 +2,22 @@ import React, { createContext, FC, useContext, useMemo } from 'react';
 
 export type ParentComponentsContextType = string[];
 
+/**
+ * Context for tracking parent elements in the React component tree.
+ */
 const ParentComponentsContext = createContext<ParentComponentsContextType>([]);
 
-interface ParentComponentsProviderProps {
+export interface ParentComponentsProviderProps {
+  /**
+   * The name of the current component to register to the tracked component tree.
+   */
   componentName: string;
   children: React.ReactNode;
 }
 
+/**
+ * Provider to wrap around components you would like to check for in the component tree by a child component.
+ */
 export const ParentComponentsContextProvider: FC<
   ParentComponentsProviderProps
 > = ({ componentName, children }) => {
@@ -26,6 +35,11 @@ export const ParentComponentsContextProvider: FC<
   );
 };
 
+/**
+ * Hook to retrieve an ordered list of the tracked ancestors of the current component.
+ * Ordered from furthest -> closest ancestor.
+ * @returns The list of parent component names.
+ */
 export const useParentComponents = () => useContext(ParentComponentsContext);
 
 export default ParentComponentsContext;
