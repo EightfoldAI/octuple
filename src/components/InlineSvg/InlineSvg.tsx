866b50f99dc94531d6753e2727ebd702b88850de
Dylan Kilgore <dkilgore@eightfold.ai>
2024-05-29 10:06:24 -0700

    fix: inlinesvg: adds cross fetch dep to avoid fetch api reference errors

--- src/components/InlineSvg/InlineSvg.tsx
+++ src/components/InlineSvg/InlineSvg.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useMemo, useState, useRef, FC, Ref } from 'react';
+import fetch from 'cross-fetch';
 import { InlineSvgProps } from './InlineSvg.types';
 import { Icon, IconName } from '../Icon';
 import { Skeleton, SkeletonVariant } from '../Skeleton';
@@ -46,6 +47,15 @@ export const InlineSvg: FC<InlineSvgProps> = React.forwardRef(
         }
         try {
           const response: Response = await fetch(_url);
+
+          // If the response is not ok (404, 500 ...), set the error state and return
+          if (!response.ok) {
+            console.error(response.status);
+            setHasError(true);
+            setIsLoading(false);
+            return;
+          }
+
           const text: string = await response.text();
 
           const parser: DOMParser = new DOMParser();
@@ -59,6 +69,7 @@ export const InlineSvg: FC<InlineSvgProps> = React.forwardRef(
           svgRef.current.innerHTML = text;
           setIsLoading(false);
         } catch (error) {
+          // Set the error state upon network error rejection of the try block
           console.error(error);
           setHasError(true);
           setIsLoading(false);
