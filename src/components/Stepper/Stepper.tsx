da5e5c3529a36acae715c44b4e82a31b8ec67416
cpatil-c-eightfold <cpatil-c@eightfold.ai>
2024-08-24 00:45:58 +0530

    fix: make stepper region focusable if scrollable (#876)
    * fix: stepper region: fixed issue by making stepper region focusable
    
    * fix: stepper region: updated test snapshot for stepper
    
    * fix: stepper region: updated tabindex attribute condition
    
    * fix: stepper region: added test case for scrollable stepper
    
    ---------
    
    Co-authored-by: Jeff Howard <106610186+jhoward-eightfold@users.noreply.github.com>

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -522,6 +522,7 @@ export const Stepper: FC<StepperProps> = React.forwardRef(
                   className={styles.stepsContainer}
                   onScroll={handleScroll}
                   ref={stepsContainerRef}
+                  tabIndex={mergedScrollable ? 0 : null}
                 >
                   <ul className={styles.steps} ref={stepsRef}>
                     {steps.map((step: Step, index: number) => {
