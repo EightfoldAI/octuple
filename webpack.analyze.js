69779fcf4ace322be97b51b6a680eaffc6c70120
Anshul Nema <anema@eightfold.ai>
2024-09-12 11:10:46 +0530

    feat: allow generating css sourcemaps

--- webpack.analyze.js
+++ webpack.analyze.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const WebpackCommonConfig = require('./webpack.common');
 
 process.env.NODE_ENV = 'development';
@@ -7,15 +9,15 @@ module.exports = (_, argv) => {
   const commonConfig = WebpackCommonConfig(_, argv);
   return {
     ...commonConfig,
-    mode: 'development',
-    externals: {
-      react: 'commonjs react',
-      'react-dom': 'commonjs react-dom',
-    },
     plugins: [
       ...commonConfig.plugins,
       new webpack.DefinePlugin({
         VERSION: JSON.stringify(require('./package.json').version),
+      }),
+      new BundleAnalyzerPlugin({
+        options: {
+          generateStatsFile: true,
+        },
       }),
     ],
   };
