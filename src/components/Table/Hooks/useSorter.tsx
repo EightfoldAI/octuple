8febd8950fb43258b00409b2951dd641b4aea314
sghosh-c-eightfold <sghosh-c@eightfold.ai>
2025-01-15 17:28:36 +0530

    fix: table cells access using shortcut key

--- src/components/Table/Hooks/useSorter.tsx
+++ src/components/Table/Hooks/useSorter.tsx
@@ -175,7 +175,33 @@ function injectSorter<RecordType>(
         ]),
         title: (renderProps: ColumnTitleProps<RecordType>) => {
           const renderSortTitle = (
-            <div className={styles.tableColumnSorters}>
+            <div 
+              className={mergeClasses([
+                styles.tableColumnSorters,
+                styles.tableColumnHasSorters,
+              ])}
+              onClick={(event: React.MouseEvent<HTMLElement>) => {
+                triggerSorter({
+                  column,
+                  key: columnKey,
+                  sortOrder: nextSortOrder,
+                  multiplePriority: getMultiplePriority(column),
+                });
+              }}
+              onKeyDown={(event: React.KeyboardEvent<HTMLElement>) => {
+                if (event.key === eventKeys.ENTER) {
+                  triggerSorter({
+                    column,
+                    key: columnKey,
+                    sortOrder: nextSortOrder,
+                    multiplePriority: getMultiplePriority(column),
+                  });
+                }
+              }}
+              tabIndex={0}
+              role="button"
+              data-sort-order={sorterOrder}
+            >
               <span className={styles.tableColumnTitle}>
                 {renderColumnTitle(column.title, renderProps)}
               </span>
@@ -214,48 +240,13 @@ function injectSorter<RecordType>(
         onHeaderCell: (col) => {
           const cell: React.HTMLAttributes<HTMLElement> =
             (column.onHeaderCell && column.onHeaderCell(col)) || {};
-          const originOnClick = cell.onClick;
-          const originOKeyDown = cell.onKeyDown;
-          cell.onClick = (event: React.MouseEvent<HTMLElement>) => {
-            triggerSorter({
-              column,
-              key: columnKey,
-              sortOrder: nextSortOrder,
-              multiplePriority: getMultiplePriority(column),
-            });
-            originOnClick?.(event);
+          return {
+            ...cell,
+            className: mergeClasses([
+              cell.className,
+              styles.tableColumnHasSorters,
+            ])
           };
-          cell.onKeyDown = (event: React.KeyboardEvent<HTMLElement>) => {
-            if (event.key === eventKeys.ENTER) {
-              triggerSorter({
-                column,
-                key: columnKey,
-                sortOrder: nextSortOrder,
-                multiplePriority: getMultiplePriority(column),
-              });
-              originOKeyDown?.(event);
-            }
-          };
-
-          // Inform the screen-reader so it can tell the visually impaired user which column is sorted
-          if (sorterOrder) {
-            if (sorterOrder === 'ascend') {
-              cell['aria-sort'] = 'ascending';
-            } else {
-              cell['aria-sort'] = 'descending';
-            }
-          }
-
-          // Ensures the cell has the proper role.
-          cell.role = 'button';
-
-          cell.className = mergeClasses([
-            cell.className,
-            styles.tableColumnHasSorters,
-          ]);
-          cell.tabIndex = 0;
-
-          return cell;
         },
       };
     }
