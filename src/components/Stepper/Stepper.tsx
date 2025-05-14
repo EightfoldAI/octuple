94348a26db3fe2e92cf9fcce57e73c6ed9ef4702
krane-c-eightfold <krane-c@eightfold.ai>
2025-05-14 10:58:31 +0530

    Revert "fix: stepper: added aria-current (#974)"
    This reverts commit b4e1bc377e33c2c80bdf3bc1a1c65aeba209022e.

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -802,7 +802,7 @@ export const Stepper: FC<StepperProps> = React.forwardRef(
                             size === StepperSize.Small &&
                             layout === 'horizontal' && (
                               <hr
-                                aria-hidden="true"
+                                aria-hidden='true'
                                 className={mergeClasses([
                                   styles.separator,
                                   {
@@ -821,15 +821,10 @@ export const Stepper: FC<StepperProps> = React.forwardRef(
                                 ])}
                               />
                             )}
-                          <li
-                            {...(index === currentActiveStep && {
-                              'aria-current': 'step',
-                            })}
-                            className={styles.step}
-                          >
+                          <li className={styles.step}>
                             {layout === 'vertical' && (
                               <hr
-                                role="presentation"
+                                role='presentation'
                                 className={mergeClasses([
                                   innerSeparatorClassNames,
                                   (styles as any)[`${step.size}`],
@@ -840,7 +835,7 @@ export const Stepper: FC<StepperProps> = React.forwardRef(
                             )}
                             {size !== StepperSize.Small && (
                               <hr
-                                role="presentation"
+                                role='presentation'
                                 className={mergeClasses([
                                   innerSeparatorClassNames,
                                   (styles as any)[`${step.size}`],
