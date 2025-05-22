7832ca71f689b55f887f4e7940de9620bf9c10f3
Rishab Motwani <rmotwani-c@eightfold.ai>
2025-05-22 16:40:16 +0530

    fix: snackbar: fixed focus flow

--- src/components/Snackbar/Snackbar.tsx
+++ src/components/Snackbar/Snackbar.tsx
@@ -1,31 +1,30 @@
 'use client';
 
-import React, { FC, useRef } from 'react';
+import React, { FC, forwardRef, useEffect, useRef } from 'react';
 import { InfoBar } from '../InfoBar';
 import { SnackbarProps } from './Snackbar.types';
 import { mergeClasses } from '../../shared/utilities';
 
 import styles from './snackbar.module.scss';
+import { useMergedRefs } from '../../hooks/useMergedRefs';
 
-export const Snackbar: FC<SnackbarProps> = ({
-  classNames,
-  moveFocusToSnackbar,
-  ...rest
-}) => {
-  const snackbarClasses = mergeClasses([styles.snackbar, classNames]);
-  const snackbarRef = useRef<HTMLDivElement>(null);
-  return (
-    <InfoBar
-      role="status"
-      ref={snackbarRef}
-      moveFocusToSnackbar={moveFocusToSnackbar}
-      moveFocusToCloseButton={moveFocusToCloseButton}
-      tabIndex={0}
-      {...rest}
-      classNames={snackbarClasses}
-      contentClassNames={styles.content}
-      contentWrapperClassNames={styles.contentWrapper}
-      actionButtonClassNames={styles.actionButton}
-    />
-  );
-};
+export const Snackbar: FC<SnackbarProps> = forwardRef(
+  ({ classNames, moveFocusToSnackbar, ...rest }, parentRef) => {
+    const snackbarClasses = mergeClasses([styles.snackbar, classNames]);
+    const snackbarRef = useRef<HTMLDivElement>(null);
+    const mergedRef = useMergedRefs(parentRef, snackbarRef);
+
+    return (
+      <InfoBar
+        role="status"
+        ref={mergedRef}
+        moveFocusToSnackbar={moveFocusToSnackbar}
+        {...rest}
+        classNames={snackbarClasses}
+        contentClassNames={styles.content}
+        contentWrapperClassNames={styles.contentWrapper}
+        actionButtonClassNames={styles.actionButton}
+      />
+    );
+  }
+);
