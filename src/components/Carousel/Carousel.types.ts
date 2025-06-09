a26e280a266930fb2caf5e370e9eb032b1605efc
sthakur-c-2-eightfold <sthakur-c@eightfold.ai>
2025-06-09 20:19:02 +0530

    fix: carousel: docs added for carouselOuterContainerProps prop

--- src/components/Carousel/Carousel.types.ts
+++ src/components/Carousel/Carousel.types.ts
@@ -401,7 +401,9 @@ export interface ScrollMenuProps
    * See https://gist.github.com/gre/1650294#file-easing-js
    */
   transitionEase?: (t: number) => number;
-
+  /**
+   * Props for the carousel outer container.
+   */
   carouselOuterContainerProps?: React.HTMLAttributes<HTMLDivElement>;
 }
 
