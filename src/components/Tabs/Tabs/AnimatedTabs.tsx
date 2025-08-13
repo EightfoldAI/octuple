f5537a9097f5371cd0d65ec06d6ddc3ceab1ff02
Muskan Bhagat <mbhagat-c@eightfold.ai>
2025-08-13 14:12:35 +0530

    fix: enableArrowNav default prop error from animated tabs

--- src/components/Tabs/Tabs/AnimatedTabs.tsx
+++ src/components/Tabs/Tabs/AnimatedTabs.tsx
@@ -27,6 +27,7 @@ export const AnimatedTabs: FC<TabsProps> = React.forwardRef(
       style,
       underlined = false,
       variant = TabVariant.default,
+      enableArrowNav = false,
       ...rest
     },
     ref: Ref<HTMLDivElement>
