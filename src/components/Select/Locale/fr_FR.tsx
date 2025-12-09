c3138f46413995988a9c30aebb5106fd2102d413
Sannidhi Sherigar <ssherigar-c@eightfold.ai>
2025-12-09 20:55:11 +0530

    fix(Select): accessibility fix included aria-label for the selected option (#1058)
    * fix: accessibility fix included aria-label for the selected option
    
    * fix: added translation file
    
    * fix: created a function for arialabel value
    
    ---------
    
    Co-authored-by: Sannidhi Sherigar <ssherigar-c@IND456-JD9DVJCV66-MacBook-Pro.local>

---
+++ src/components/Select/Locale/fr_FR.tsx
@@ -0,0 +1,10 @@
+import type { SelectLocale } from '../Select.types';
+
+const locale: SelectLocale = {
+  lang: {
+    locale: 'fr_FR',
+    currentlySelectedText: 'option actuellement sélectionnée',
+  },
+};
+
+export default locale;
---
+++ src/components/Select/Locale/ru_RU.tsx
@@ -0,0 +1,10 @@
+import type { SelectLocale } from '../Select.types';
+
+const locale: SelectLocale = {
+  lang: {
+    locale: 'ru_RU',
+    currentlySelectedText: 'текущий выбранный вариант',
+  },
+};
+
+export default locale;
