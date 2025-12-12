1dc8501adfbb50881a16ad52e80297fa117c0496
Kris Heinrich <kheinrich@eightfold.ai>
2025-12-11 16:23:11 -0800

    ENG-163062: Fix alt text for decorative images in Empty component (#1049)
    * Fix alt text for decorative images in Empty
    
    * fix(a11y): add tests for decorative images
    
    ---------
    
    Co-authored-by: Yash raj chhabra <95337653+ychhabra-eightfold@users.noreply.github.com>

--- src/components/Empty/Empty.test.tsx
+++ src/components/Empty/Empty.test.tsx
@@ -45,4 +45,23 @@ describe('Empty', () => {
     const fillValue = pathElement.getAttribute('fill');
     expect(fillValue).toBe('currentColor');
   });
+
+  test('empty with default decorative SVG has aria-hidden="true"', async () => {
+    const { container } = render(<Empty description="Test" />);
+    const svgElement = container.querySelector('svg');
+    expect(svgElement).toBeInTheDocument();
+    expect(svgElement).toHaveAttribute('aria-hidden', 'true');
+  });
+
+  test('empty with external decorative image has empty alt text', async () => {
+    const { container } = render(
+      <Empty
+        description="Test"
+        image="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/car.svg"
+      />
+    );
+    const imgElement = container.querySelector('img');
+    expect(imgElement).toBeInTheDocument();
+    expect(imgElement).toHaveAttribute('alt', '');
+  });
 });
