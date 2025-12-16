285683edccdf281bb2d59ad7a508c44693fed8ae
Mohammed Fazial <mfazil@eightfold.ai>
2025-12-16 15:08:22 +0530

    chore: fixes types

--- src/components/Tabs/Stat/Stat.tsx
+++ src/components/Tabs/Stat/Stat.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { FC, Ref, useContext } from 'react';
+import React, { Ref, useContext } from 'react';
 import GradientContext, {
   Gradient,
 } from '../../ConfigProvider/GradientContext';
@@ -19,7 +19,10 @@ const MEDIUM_ICON_SIZE: string = '40px';
 const SMALL_ICON_SIZE: string = '32px';
 const XSMALL_ICON_SIZE: string = '24px';
 
-export const Stat: FC<StatProps> = React.forwardRef(
+export const Stat = React.forwardRef<
+  HTMLButtonElement | HTMLDivElement,
+  StatProps
+>(
   (
     {
       ariaLabel,
@@ -44,7 +47,7 @@ export const Stat: FC<StatProps> = React.forwardRef(
       interactive = true,
       ...rest
     },
-    ref: Ref<HTMLButtonElement>
+    ref: Ref<HTMLButtonElement | HTMLDivElement>
   ) => {
     const htmlDir: string = useCanvasDirection();
 
@@ -161,8 +164,8 @@ export const Stat: FC<StatProps> = React.forwardRef(
     if (!interactive) {
       return (
         <div
-          {...rest}
-          ref={ref}
+          {...(rest as unknown as React.HTMLAttributes<HTMLDivElement>)}
+          ref={ref as unknown as Ref<HTMLDivElement>}
           className={tabClassName}
           style={{ ...rest.style, maxWidth }}
         >
@@ -174,7 +177,7 @@ export const Stat: FC<StatProps> = React.forwardRef(
     return (
       <button
         {...rest}
-        ref={ref}
+        ref={ref as Ref<HTMLButtonElement>}
         className={tabClassName}
         aria-label={ariaLabel}
         aria-selected={!readOnly && isActive}
