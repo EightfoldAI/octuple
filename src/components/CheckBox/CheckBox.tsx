3bf407e957d06a87a8739174adf98c12810d974a
jmundhra-eightfold <jmundhra@eightfold.ai>
2025-12-22 22:18:44 +0530

    fixes

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -208,7 +208,7 @@ export const CheckBox: FC<CheckboxProps> = React.forwardRef(
             name={name}
             type={'checkbox'}
             value={value}
-            readOnly={!disabled}
+            readOnly={!toggle}
             role={toggle ? 'switch' : 'checkbox'}
             {...rest}
           />
