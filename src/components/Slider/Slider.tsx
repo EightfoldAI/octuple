92a15acc8509a48d2d3b8ab4a8a294bfdab04c5a
Vignaraj <vpai@eightfold.ai>
2026-01-09 15:53:10 +0530

    init

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -986,7 +986,7 @@ export const Slider: FC<SliderProps> = React.forwardRef(
                       type="range"
                       readOnly={readOnly}
                       step={mergedStep}
-                      tabIndex={readOnly ? -1 : 0}
+                      tabIndex={readOnly ? -1 : undefined}
                       value={val}
                     />
                   </Tooltip>
